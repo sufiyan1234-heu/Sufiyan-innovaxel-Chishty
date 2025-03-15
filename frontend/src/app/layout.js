59ce66343d6c85b7386de6d1a9465e69d4988c1e
Sufiyan <sufiyanchishty71@gmail.com>
2025-03-16 02:16:58 +0500

    create navbar component , layout , landing page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
+        <main >
+        <Toaster />
         {children}
+        </main>
       </body>
     </html>
   );
